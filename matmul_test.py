a9f9c0572c7b76b310994f5cbc5289a4d19d0314
Leqr <leonardeqr@icloud.com>
2022-01-08 00:03:17 +0100

    tensorflow matmul fix

--- matmul_test.py
+++ matmul_test.py
@@ -23,18 +23,17 @@ for i in range(n):
     A_torch = A_torch*A_torch
 t_torch = time.perf_counter() - start_torch
 print(f"Torch time : {t_torch}")
+
 start_tf = time.perf_counter()
 for i in range(n):
-    A_tensor = A_tensor*A_tensor
+    A_tensor = A_tensor@A_tensor
 t_tf = time.perf_counter() - start_tf
 print(f"Tensorflow time : {t_tf}")
-
 
 start_np = time.perf_counter()
 for i in range(n):
     A_numpy = A_numpy@A_numpy
 t_np = time.perf_counter() - start_np
-
 print(f"Numpy time : {t_np}")
 
 data = [["PyTorch",str(round(t_torch,6))+"s"],["Tensorflow",str(round(t_tf,6))+"s"],["Numpy",str(round(t_np,6))+"s"]]
